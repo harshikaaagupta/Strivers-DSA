65b005bfcd9bdab357c7cb052e73e584ddcb7377
Harshika Gupta <harshikagupta098@gmail.com>
2025-09-16 12:08:54 +0530

    patterns

---
+++ Patterns/ques6.java
@@ -0,0 +1,11 @@
+public class ques6 {
+    public static void main(String args[]){
+        for(int i = 1;i<=5;i++){
+            for(int j =5;j>=i;j--){
+                System.out.print(5-j+1+"");
+            }
+            System.out.println("");
+        }
+    }
+    
+}
