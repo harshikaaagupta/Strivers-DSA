30a93e8495c04851cb969a9f2651c7800dcf0c76
Harshika Gupta <harshikagupta098@gmail.com>
2025-09-19 16:32:35 +0530

    patterns

---
+++ Patterns/diamond.java
@@ -0,0 +1,23 @@
+public class diamond {
+    public static void main(String args[]){
+        for(int i = 1;i<=5;i++){
+            for(int s = 1;s<=5-i;s++){
+                System.out.print(" ");
+            }
+            for(int j = 1;j<=2*i-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i = 4;i>=1;i--){
+            for(int s = 1;s<=5-i;s++){
+                System.out.print(" ");
+            }
+            for(int j = 1;j<=2*i-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
