d12e9e3db1ce562420e9e4423d9a02e71e19b49b
Harshika Gupta <harshikagupta098@gmail.com>
2025-09-19 16:26:17 +0530

    patterns

---
+++ Patterns/reverseTriangle.java
@@ -0,0 +1,15 @@
+public class reverseTriangle {
+    public static void main(String[] args) {
+        for(int i = 5;i>=1;i--){
+            for(int s=0;s<=5-i;s++){
+                System.out.print(" ");
+            }
+            for(int j = 1;j<=2*i-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
+    
+}
